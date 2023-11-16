ec097583546ba035127f77875f080229777a1726
Ke1ha <dmitrij132005@gmail.com>
2023-11-16 09:56:39 +0300

    1231

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,3 +4,10 @@ Console.WriteLine("Всегда делали!");
 
 Console.WriteLine("А когда не делали мага");
 Console.WriteLine("Всегда делали!");
+Console.WriteLine("А когда не делали мага");
+Console.WriteLine("Всегда делали!");
+Console.WriteLine("А когда не делали мага");
+Console.WriteLine("Всегда делали!");
+
+Console.WriteLine("А когда не делали мага");
+Console.WriteLine("Всегда делали!");
